90f02d03de5c037d9c9b72967503471e07339ed2
fescii <isfescii@gmail.com>
2023-09-10 18:29:03 +0300

    Booking Modal Entry A41

--- js/modal.book.js
+++ js/modal.book.js
@@ -69,6 +69,20 @@ export default class ModalBook extends HTMLElement {
         <div id="container" class="container">
           ${this.getStepOne()}
         </div>
+        <div class="footer">
+          <div class="action prev">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M15.4992 19.7504C15.3692 19.7504 15.2382 19.7174 15.1182 19.6464C14.3642 19.1994 7.75024 15.1914 7.75024 12.0004C7.75024 8.81043 14.3632 4.80143 15.1182 4.35443C15.4732 4.14343 15.9352 4.26143 16.1452 4.61843C16.3562 4.97543 16.2382 5.43543 15.8822 5.64643C13.3182 7.16543 9.25024 10.2334 9.25024 12.0004C9.25024 13.7704 13.3182 16.8374 15.8822 18.3544C16.2382 18.5654 16.3562 19.0254 16.1452 19.3824C16.0052 19.6184 15.7562 19.7504 15.4992 19.7504Z" fill="black"/>
+            </svg>
+            <span class="text">Previous</span>
+          </div>
+          <div class="action next">
+            <span class="text">Next</span>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M8.50076 19.7504C8.63076 19.7504 8.76176 19.7174 8.88176 19.6464C9.63576 19.1994 16.2498 15.1914 16.2498 12.0004C16.2498 8.81043 9.63676 4.80143 8.88176 4.35443C8.52676 4.14343 8.06476 4.26143 7.85476 4.61843C7.64375 4.97543 7.76176 5.43543 8.11776 5.64643C10.6818 7.16543 14.7498 10.2334 14.7498 12.0004C14.7498 13.7704 10.6818 16.8374 8.11776 18.3544C7.76176 18.5654 7.64375 19.0254 7.85476 19.3824C7.99476 19.6184 8.24376 19.7504 8.50076 19.7504Z" fill="black"/>
+            </svg>
+          </div>
+        </div>
       </section>
     ${this.getStyles()}`;
   }
@@ -245,6 +259,7 @@ export default class ModalBook extends HTMLElement {
         display: flex;
         flex-flow: column;
         justify-content: start;
+        align-items: center;
         gap: 0;
         width: 700px;
         min-height: 80%;
@@ -256,7 +271,6 @@ export default class ModalBook extends HTMLElement {
 
       #content .content-head{
         margin: 0 20px;
-        /*border-bottom: 1px solid #80808027;*/
         padding: 5px 0 10px 0;
         display: flex;
         flex-flow: column;
@@ -287,7 +301,6 @@ export default class ModalBook extends HTMLElement {
       #content .content-head > .actions > .control > svg{
         width: 25px;
         height: 25px;
-    
       }
 
       #content .content-head > .actions > span.steps {
@@ -313,6 +326,40 @@ export default class ModalBook extends HTMLElement {
         color: rgb(223, 121, 26);
       }
 
+      section#content > .footer {
+        margin: 30px 0 0 0;
+        width: 80%;
+        padding: 5px 0 10px 0;
+        display: flex;
+        flex-flow: row;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      section#content > .footer > .action {
+        border: 1px solid #808080;
+        display: flex;
+        flex-flow: row;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 50px;
+        font-family: var(--font-alt);
+        line-height: 1.2;
+        font-size: 1.2rem;
+        font-weight: 500;
+        color: #404040;
+        cursor: pointer;
+      }
+
+      section#content > .footer > .action.prev {
+        padding: 10px 20px 10px 15px;
+      }
+
+      section#content > .footer > .action.next {
+        padding: 10px 15px 10px 20px;
+      }
+
       section#content > .container {
         /*border: 2px solid #808080;*/
         padding: 5px 0 10px 0;
@@ -324,6 +371,7 @@ export default class ModalBook extends HTMLElement {
 
        section#content > .container > .head {
         /*border: 1px solid blue;*/
+        width: 80%;
         margin: 0 0 30px 0;
         display: flex;
         flex-flow: column;

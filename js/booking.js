73ba71d2eee12a1dba1e2451dcac19c5144614af
fescii <isfescii@gmail.com>
2023-09-27 11:14:50 +0300

    Dashboard - Entry A224

--- js/booking.js
+++ js/booking.js
@@ -50,16 +50,16 @@ export default class bookingContainer extends HTMLElement {
       <div class="head">
         <div class="left">
           <div class="date">
-            <span class="day">Mon</span>
-            <span class="no">20</span>
+            <span class="day">${this.getAttribute('date-day')}</span>
+            <span class="no">${this.getAttribute('date-date')}</span>
           </div>
           <div class="quick-info">
             <span class="top">
-              <span class="month">Jul</span>
+              <span class="month">${this.getAttribute('date-month')}</span>
               <span class="dot"></span>
-              <span class="service">Wedding</span>
+              <span class="service">${this.getAttribute('service')}</span>
             </span>
-            <p class="location">Nairobi area, near city park</p>
+            <p class="location">${this.getAttribute('location')}</p>
           </div>
         </div>
         <div class="right">
@@ -79,23 +79,23 @@ export default class bookingContainer extends HTMLElement {
           <div class="infos">
             <div class="info name">
               <h3 class="label">Name</h3>
-              <p class="text">Fredrick Ochieng</p>
+              <p class="text">${this.getAttribute('full-name')}</p>
             </div>
             <div class="info email">
               <h3 class="label">Phone</h3>
-              <p class="text">+254713253018</p>
+              <p class="text">${this.getAttribute('phone')}</p>
             </div>
             <div class="info number">
               <h3 class="label">Email</h3>
-              <p class="text">isfescii@gmail.com</p>
+              <p class="text">${this.getAttribute('email')}</p>
             </div>
             <div class="info package">
               <h3 class="label">Package</h3>
-              <p class="text">Package II</p>
+              <p class="text">${this.getAttribute('package')}</p>
             </div>
             <div class="info other">
               <h3 class="label">Other</h3>
-              <p class="text">Lorem ipsum dolor sit amet consecrate, adipisicing elit. Fuga, reprehenderit iusto! Quidem earum distinctio, tenetur nesciunt sed aperiam libero beatae sequi?</p>
+              <p class="text">${this.getAttribute('other')}</p>
             </div>
           </div>
         </div>
@@ -103,7 +103,7 @@ export default class bookingContainer extends HTMLElement {
           <div class="infos">
             <div class="info coverage">
               <h3 class="label">Coverage</h3>
-               <p class="info">About 50 - 100 people to be covered</p>
+               <p class="info">${this.getAttribute('coverage')}</p>
             </div>
           </div>
         </div>
@@ -118,208 +118,213 @@ export default class bookingContainer extends HTMLElement {
         box-sizing: border-box !important;
       }
 
-    :host {
-      /* border: 1px solid #ce0404; */
-      border-bottom: 1px solid #80808027;
-      position: relative;
-      padding: 10px 5px;
-      width: 100%;
-      display: flex;
-      flex-flow: column;
-      align-items: center;
-      justify-content: center;
-      gap: 0;
-      /* border-radius: 18px; */
-    }
+      :host {
+        /* border: 1px solid #ce0404; */
+        border-bottom: 1px solid #80808027;
+        position: relative;
+        padding: 10px 5px;
+        width: 100%;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+        gap: 0;
+        /* border-radius: 18px; */
+      }
 
-    .head {
-      /* border: 1px solid #81ce04; */
-      padding: 0;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      gap: 0;
-    }
+      .head {
+        /* border: 1px solid #81ce04; */
+        padding: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 0;
+      }
 
-    .head > .left {
-      /* border: 1px solid #81ce04; */
-      padding: 0;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      gap: 15px;
-    }
+      .head > .left {
+        /* border: 1px solid #81ce04; */
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 15px;
+      }
 
-    .head > .left > .date {
-      /* border: 1px solid #808080; */
-      background-color: #f5f5f5;
-      padding: 8px 15px;
-      display: flex;
-      flex-flow: column;
-      align-items: center;
-      justify-content: center;
-      gap: 3px;
-      border-radius: 15px;
-    }
+      .head > .left > .date {
+        /* border: 1px solid #808080; */
+        background-color: #f5f5f5;
+        padding: 8px 15px;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+        gap: 3px;
+        border-radius: 15px;
+      }
 
-    .head > .left > .date > span.day {
-      margin: 0;
-      color: #808080;
-      font-family: var(--font-alt);
-      font-size: 0.85rem;
-      line-height: 1;
-    }
+      .head > .left > .date > span.day {
+        margin: 0;
+        color: #808080;
+        font-family: var(--font-alt);
+        font-size: 0.85rem;
+        line-height: 1;
+      }
 
-    .head > .left > .date >  span.no {
-      margin: 0;
-      display: inline-block;
-      color:  #404040;
-      font-family: var(--font-alt);
-      font-weight: 500;
-      font-size: 1.4rem;
-      line-height: 1;
-    }
+      .head > .left > .date >  span.no {
+        margin: 0;
+        display: inline-block;
+        color:  #404040;
+        font-family: var(--font-alt);
+        font-weight: 500;
+        font-size: 1.4rem;
+        line-height: 1;
+      }
 
-    .head > .left .quick-info {
-      /* border: 1px solid #808080; */
-      margin: -3px 0 0 0;
-      display: flex;
-      flex-flow: column;
-      align-items: start;
-      justify-content: start;
-      gap: 5px;
-    }
+      .head > .left .quick-info {
+        /* border: 1px solid #808080; */
+        margin: -3px 0 0 0;
+        display: flex;
+        flex-flow: column;
+        align-items: start;
+        justify-content: start;
+        gap: 5px;
+      }
 
-    .head > .left .quick-info > .top {
-      /* border: 1px solid #808080; */
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 5px;
-      font-size: 0.9rem;
-      font-family: var(--font-alt);
-      color: #808080;
-    }
+      .head > .left .quick-info > .top {
+        /* border: 1px solid #808080; */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+        font-size: 0.9rem;
+        font-family: var(--font-alt);
+        color: #808080;
+      }
 
-    .head > .left .quick-info > .top .month {
-      /* color: #08b86f; */
-      color: #808080;
-      /* font-family: inherit; */
-      font-size: 1rem;
-    }
-
-
-    .head > .left .quick-info > .top .service {
-      /* color: #08b86f; */
-      color: #606060;
-      font-family: inherit;
-    }
-
-    .head > .left .quick-info > .top .dot {
-      padding: 0;
-      margin: 2px 0 0 0;
-      display: flex;
-      width: 4px;
-      height: 4px;
-      gap: 5px;
-      font-size: 0.8rem;
-      background-color: #808080;
-      border-radius: 5px;
-      -webkit-border-radius: 5px;
-    }
-
-    .head > .left .quick-info  p.location {
-      margin: 0;
-      color: #404040;
-      font-family: var(--font-alt);
-      /* font-weight: 500; */
-      font-size: 1.1rem;
-    }
-
-    .head > .right {
-      /* border: 1px solid #80808027; */
-      padding: 5px 8px 5px 10px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      gap: 8px;
-      cursor: pointer;
-      color: #808080;
-      fill: #808080;
-      border-radius: 20px;
-    }
-
-    .head > .right > span.text {
-      color: inherit;
-    }
-
-    .head > .right > svg {
-      fill: inherit;
-    }
-
-    .head > .right:hover {
-      color: #08b86f;
-      fill: #08b86f;
-    }
-
-    .content {
-      border-top: 1px dashed #80808047;
-      margin: 16px 0 0 0;
-      padding: 10px 0 0 0;
-      width: 100%;
-      display: flex;
-      flex-flow: column;
-      align-items: center;
-      justify-content: space-between;
-      gap: 8px;
-    }
-
-    .content > .section {
-      /* border-top: 1px solid #80808017; */
-      display: flex;
-      flex-flow: column;
-      gap: 8px;
-      width: 100%;
-    }
-
-    .content > .section.bio > .infos {
-      /* border: 1px solid #808080; */
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      row-gap: 15px;
-    }
-
-    .content > .section .infos > .info {
-      /* border: 1px solid rgba(182, 19, 19, 0.09); */
-      display: flex;
-      flex-flow: column;
-      gap: 0;
-    }
+      .head > .left .quick-info > .top .month {
+        /* color: #08b86f; */
+        color: #808080;
+        /* font-family: inherit; */
+        font-size: 1rem;
+      }
 
 
-    .content > .section .infos > .info.other {
-      grid-column: 1/3;
-    }
+      .head > .left .quick-info > .top .service {
+        /* color: #08b86f; */
+        color: #606060;
+        font-family: inherit;
+      }
 
-    .content > .section .infos > .info > h3.label {
-      margin: 0;
-      color: #404040;
-      font-family: var(--font-alt);
-      font-size: 1.09rem;
-      font-weight: 500;
-    }
+      .head > .left .quick-info > .top .dot {
+        padding: 0;
+        margin: 2px 0 0 0;
+        display: flex;
+        width: 4px;
+        height: 4px;
+        gap: 5px;
+        font-size: 0.8rem;
+        background-color: #808080;
+        border-radius: 5px;
+        -webkit-border-radius: 5px;
+      }
 
-    .content > .section .infos > .info > p {
-      margin: 0;
-      color: #404040;
-      font-family: var(--font-alt);
-      font-size: 1rem;
-      font-weight: 400;
-      line-height: 1.4;
-    }
+      .head > .left .quick-info  p.location {
+        margin: 0;
+        color: #404040;
+        font-family: var(--font-alt);
+        /* font-weight: 500; */
+        font-size: 1.1rem;
+      }
+
+      .head > .right {
+        /* border: 1px solid #80808027; */
+        padding: 5px 8px 5px 10px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 8px;
+        cursor: pointer;
+        color: #808080;
+        fill: #808080;
+        border-radius: 20px;
+      }
+
+      .head > .right > span.text {
+        color: inherit;
+      }
+
+      .head > .right > svg {
+        fill: inherit;
+      }
+
+      .head > .right:hover {
+        color: #08b86f;
+        fill: #08b86f;
+      }
+
+      .content {
+        border-top: 1px dashed #80808047;
+        margin: 16px 0 0 0;
+        padding: 10px 0 0 0;
+        width: 100%;
+        display: none;
+        flex-flow: column;
+        align-items: center;
+        justify-content: space-between;
+        gap: 12px;
+      }
+
+      .content > .section {
+        /* border-top: 1px solid #80808017; */
+        display: flex;
+        flex-flow: column;
+        gap: 8px;
+        width: 100%;
+      }
+
+      .content > .section.coverage {
+        /* border-top: 1px solid #80808017; */
+        padding: 0 0 10px 0;
+      }
+
+      .content > .section.bio > .infos {
+        /* border: 1px solid #808080; */
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        row-gap: 15px;
+      }
+
+      .content > .section .infos > .info {
+        /* border: 1px solid rgba(182, 19, 19, 0.09); */
+        display: flex;
+        flex-flow: column;
+        gap: 0;
+      }
+
+
+      .content > .section .infos > .info.other {
+        grid-column: 1/3;
+      }
+
+      .content > .section .infos > .info > h3.label {
+        margin: 0;
+        color: #404040;
+        font-family: var(--font-alt);
+        font-size: 1.09rem;
+        font-weight: 500;
+      }
+
+      .content > .section .infos > .info > p {
+        margin: 0;
+        color: #404040;
+        font-family: var(--font-alt);
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.4;
+      }
       
-      </style>
+    </style>
     `
   }
 }

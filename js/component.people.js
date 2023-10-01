7632bce1d1dad1fe4b7aba907a9d742b45d22ecf
fescii <isfescii@gmail.com>
2023-10-01 14:36:02 +0300

    Dashboard - Entry 311

--- js/component.people.js
+++ js/component.people.js
@@ -19,6 +19,38 @@ export default class PeopleContainer extends HTMLElement {
   connectedCallback() {
     // console.log('We are inside connectedCallback');
 
+    this.switchTabs()
+  }
+
+
+  switchTabs() {
+    const tabs = this.shadowObj.querySelectorAll('.header > span.option')
+    let activeTab = this.shadowObj.querySelector('.header > span.option.active')
+    const contentContainer = this.shadowObj.querySelector('#content-container')
+    if (tabs && contentContainer) {
+      tabs.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+          e.preventDefault()
+          e.stopPropagation()
+
+          activeTab.classList.remove('active')
+          tab.classList.add('active')
+          activeTab = tab
+
+          switch (tab.dataset.name) {
+            case 'people':
+              contentContainer.innerHTML = this.getPeople()
+              break;
+            case 'add':
+              contentContainer.innerHTML = this.addPerson()
+              break;
+            default:
+              contentContainer.innerHTML = this.getPeople()
+              break;
+          }
+        })
+      });
+    }
   }
 
 
@@ -44,18 +76,28 @@ export default class PeopleContainer extends HTMLElement {
   getHeader() {
     return `
       <div class="header">
-        <div class="left">
-          <p class="info">List of photographers</p>
-        </div>
-        <div class="right">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        <span class="option active" data-name="people">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9729 12.1402H11.9989C14.5359 12.1402 16.5999 10.0772 16.5999 7.54116C16.5999 5.00416 14.5359 2.94116 11.9989 2.94116C9.46294 2.94116 7.39894 5.00416 7.39894 7.53916C7.39294 10.0682 9.44294 12.1322 11.9729 12.1402ZM8.89894 7.54116C8.89894 5.83216 10.2899 4.44116 11.9989 4.44116C13.7089 4.44116 15.0999 5.83216 15.0999 7.54116C15.0999 9.25016 13.7089 10.6402 11.9989 10.6402H11.9749C10.2739 10.6342 8.89494 9.24516 8.89894 7.54116Z" fill="black"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.24316 17.8573C5.24316 21.0593 10.3282 21.0593 11.9992 21.0593C13.6702 21.0593 18.7552 21.0593 18.7552 17.8403C18.7552 15.4133 15.7252 13.4373 11.9992 13.4373C8.27316 13.4373 5.24316 15.4193 5.24316 17.8573ZM6.74316 17.8573C6.74316 16.4793 8.99116 14.9373 11.9992 14.9373C15.0072 14.9373 17.2552 16.4703 17.2552 17.8403C17.2552 19.2613 14.3962 19.5593 11.9992 19.5593C9.60216 19.5593 6.74316 19.2643 6.74316 17.8573Z" fill="black"/>
+            <path d="M18.3897 11.0827C18.0577 11.0827 17.7547 10.8617 17.6647 10.5267C17.5577 10.1267 17.7957 9.71574 18.1957 9.60774C19.1327 9.35674 19.7877 8.50274 19.7877 7.53074C19.7877 6.51374 19.0677 5.62774 18.0737 5.42574C17.6677 5.34274 17.4067 4.94574 17.4887 4.53974C17.5727 4.13474 17.9667 3.87874 18.3737 3.95474C20.0627 4.29974 21.2877 5.80374 21.2877 7.53074C21.2877 9.18074 20.1757 10.6307 18.5837 11.0577C18.5187 11.0747 18.4527 11.0827 18.3897 11.0827Z" fill="black"/>
+            <path d="M20.1912 17.3993C20.2732 17.7453 20.5802 17.9773 20.9202 17.9773C20.9782 17.9773 21.0352 17.9703 21.0932 17.9573C22.1642 17.7053 22.9732 16.7503 22.9732 15.7373C22.9732 14.1663 21.0662 12.7363 18.9712 12.7363C18.5572 12.7363 18.2212 13.0723 18.2212 13.4863C18.2212 13.9003 18.5572 14.2363 18.9712 14.2363C20.4412 14.2363 21.4732 15.2093 21.4732 15.7373C21.4732 15.9843 21.2222 16.3863 20.7492 16.4973C20.3462 16.5923 20.0962 16.9963 20.1912 17.3993Z" fill="black"/>
+            <path d="M5.61143 11.0827C5.54843 11.0827 5.48243 11.0747 5.41843 11.0577C3.82443 10.6317 2.71143 9.18174 2.71143 7.53074C2.71143 5.80374 3.93743 4.29974 5.62643 3.95474C6.03343 3.87874 6.42843 4.13474 6.51143 4.53974C6.59343 4.94574 6.33243 5.34274 5.92643 5.42574C4.93243 5.62774 4.21143 6.51374 4.21143 7.53074C4.21143 8.50274 4.86643 9.35674 5.80443 9.60774C6.20543 9.71574 6.44243 10.1267 6.33643 10.5257C6.24643 10.8617 5.94343 11.0827 5.61143 11.0827Z" fill="black"/>
+            <path d="M2.90686 17.9573C2.96486 17.9713 3.02186 17.9773 3.07986 17.9773C3.41986 17.9773 3.72686 17.7453 3.80886 17.3993C3.90386 16.9963 3.65386 16.5913 3.25086 16.4973C2.77786 16.3863 2.52686 15.9843 2.52686 15.7373C2.52686 15.2093 3.55886 14.2363 5.02886 14.2363C5.44286 14.2363 5.77886 13.9003 5.77886 13.4863C5.77886 13.0723 5.44286 12.7363 5.02886 12.7363C2.93386 12.7363 1.02686 14.1663 1.02686 15.7373C1.02686 16.7503 1.83586 17.7063 2.90686 17.9573Z" fill="black"/>
+          </svg>
+          <span class="text">People</span>
+        </span>
+        <span class="option" data-name="add">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 22.0001C4.617 22.0001 2 19.3831 2 12.0001C2 4.61712 4.617 2.00012 12 2.00012C12.414 2.00012 12.75 2.33612 12.75 2.75012C12.75 3.16412 12.414 3.50012 12 3.50012C5.486 3.50012 3.5 5.48612 3.5 12.0001C3.5 18.5141 5.486 20.5001 12 20.5001C18.514 20.5001 20.5 18.5141 20.5 12.0001C20.5 11.5861 20.836 11.2501 21.25 11.2501C21.664 11.2501 22 11.5861 22 12.0001C22 19.3831 19.383 22.0001 12 22.0001Z" fill="black"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.2365 9.38606L20.2952 8.19072C21.4472 6.88972 21.3252 4.89472 20.0252 3.74172C19.3952 3.18372 18.5812 2.90372 17.7452 2.95572C16.9052 3.00672 16.1352 3.38272 15.5772 4.01272L9.6932 10.6607C7.8692 12.7187 9.1172 15.4397 9.1712 15.5547C9.2602 15.7437 9.4242 15.8877 9.6232 15.9497C9.6802 15.9687 10.3442 16.1717 11.2192 16.1717C12.2042 16.1717 13.4572 15.9127 14.4092 14.8367L19.0774 9.56571C19.1082 9.54045 19.1374 9.51238 19.1646 9.4815C19.1915 9.45118 19.2155 9.41925 19.2365 9.38606ZM10.4082 14.5957C11.0352 14.7097 12.4192 14.8217 13.2862 13.8427L17.5371 9.04299L15.0656 6.85411L10.8172 11.6557C9.9292 12.6567 10.2122 13.9917 10.4082 14.5957ZM16.0596 5.73076L18.5322 7.91938L19.1722 7.19672C19.7752 6.51472 19.7122 5.46872 19.0312 4.86572C18.7002 4.57372 18.2712 4.42472 17.8362 4.45272C17.3962 4.48072 16.9932 4.67672 16.7002 5.00672L16.0596 5.73076Z" fill="black"/>
           </svg>
           <span class="text">Add</span>
-        </div>
+        </span>
       </div>
     `
   }
+
 
   getPeople() {
     return `
@@ -237,38 +279,45 @@ export default class PeopleContainer extends HTMLElement {
 
       .header {
         border-bottom: 1px solid #80808017;
+        background-color: #ffffff;
+        z-index: 5;
         margin: 0;
-        padding: 20px 0;
+        padding: 30px 0 12px 0;
         width: 100%;
         display: flex;
         flex-flow: row;
         align-items: center;
-        justify-content: space-between;
-        gap: 10px;
+        justify-content: start;
+        gap: 20px;
+        position: sticky;
+        top: 80px;
       }
 
-      .header > .left > p {
-        margin: 0;
-        padding: 0;
-        font-family: var(--font-alt);
-        color: #404040;
-        font-size: 1.1rem;
-      }
-
-      .header > .right {
+      .header > span.option {
         /* border: 1px solid #80808017; */
-        background-color: rgba(20,167,62,1);
+        /* background-color: rgba(20,167,62,1); */
         padding: 5px 20px 6px 15px;
         border-radius: 50px;
         display: flex;
         align-items: center;
         justify-content: center;
         gap: 5px;
-        color: white;
+        color: #808080;
         cursor: pointer;
       }
 
-      .header > .right svg {
+      .header > span.option.active {
+        background-color: #f5f5f5;
+        color: #404040;
+        font-weight: 500;
+      }
+
+      .header > span.option:hover {
+        color: #404040;
+        font-weight: 500;
+      }
+
+      .header > span.option svg {
         /* border: 1px solid #80808017; */
         width: 24px;
         height: 24px;
@@ -277,9 +326,21 @@ export default class PeopleContainer extends HTMLElement {
         justify-content: center;
       }
 
-      .header > .right span {
+      .header > span.option svg path {
+        fill: #808080;
+      }
+
+      .header > span.option:hover svg path {
+        fill: #404040;
+      }
+
+      .header > span.option.active svg path {
+        fill: #404040;
+      }
+
+      .header > span.option span {
         font-family: var(--font-alt);
-        font-weight: 500;
+        font-weight: inherit;
       }
 
       .content {

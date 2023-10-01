5c007272570d3bf567fdb289086aa482fada5e5a
fescii <isfescii@gmail.com>
2023-10-01 16:38:18 +0300

    Dashboard - Entry 316

--- js/tab.js
+++ js/tab.js
@@ -2,38 +2,6 @@ const tabs = document.querySelectorAll('body > main.main > section.sidebar > .me
 let activeTab = document.querySelector('body > main.main > section.sidebar > .menu > ul.menu-items > li.item.active')
 const contentContainer = document.querySelector('body > main.main > section.content > #content-container')
 
-const content = {}
-
-content.overview = `
-  <stat-container bookings="163" date="Sep 23" completed="57" upcoming="33" cancelled="8" visitors="1,400"
-    interactions="1,360">
-  </stat-container>
-
-`
-
-content.schedules = `
-  <schedule-container url="some-link">
-  </schedule-container>
-`
-
-content.bookings = `
-  <booking-container url="some-link" 
-  >
-  </booking-container>
-`
-
-content.portfolio = `
-  <portfolio-container url="some-link" 
-  >
-  </portfolio-container>
-`
-
-content.people = `
-  <people-container url="some-link" 
-  >
-  </people-container>
-`
-
 if (tabs && contentContainer && activeTab) {
   tabs.forEach(tab => {
     tab.addEventListener('click', (event) => {
@@ -44,11 +12,58 @@ if (tabs && contentContainer && activeTab) {
       tab.classList.add('active')
       activeTab = tab
 
-      populateContent(contentContainer, content[tab.dataset.name])
+      populateContent(contentContainer, tab.dataset.name)
     })
   })
 }
 
-const populateContent = (contentContainer, content) => {
-  contentContainer.innerHTML = content
+
+const populateContent = (contentContainer, tab) => {
+  switch (tab) {
+    case 'overview':
+      contentContainer.innerHTML = `
+        <stat-container bookings="163" date="Sep 23" completed="57" upcoming="33" cancelled="8" visitors="1,400"
+          interactions="1,360">
+        </stat-container>
+      `
+      break;
+    case 'people':
+      contentContainer.innerHTML = `
+        <people-container url="some-link">
+        </people-container>
+      `
+      break;
+    case 'schedules':
+      contentContainer.innerHTML = `
+        <schedule-container url="some-link">
+        </schedule-container>
+      `
+      break;
+    case 'bookings':
+      contentContainer.innerHTML = `
+        <booking-container url="some-link" >
+        </booking-container>
+      `
+      break;
+    case 'settings':
+      contentContainer.innerHTML = `
+        <stat-container bookings="163" date="Sep 23" completed="57" upcoming="33" cancelled="8" visitors="1,400"
+          interactions="1,360">
+        </stat-container>
+      `
+      break;
+    case 'portfolio':
+      contentContainer.innerHTML = `
+        <portfolio-container url="some-link" >
+        </portfolio-container>
+      `
+      break;
+    case 'logout':
+      contentContainer.innerHTML = `
+       Logout
+      `
+      break;
+    default:
+      break;
+  }
 }

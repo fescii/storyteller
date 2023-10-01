ab2d60d0a7421aea7ed7f9930e7c50808b0cfe22
fescii <isfescii@gmail.com>
2023-10-01 13:14:45 +0300

    Dashboard - Entry 307

--- js/tab.js
+++ js/tab.js
@@ -1,7 +1,39 @@
 const tabs = document.querySelectorAll('body > main.main > section.sidebar > .menu > ul.menu-items > li.item')
-const activeTab = document.querySelector('body > main.main > section.sidebar > .menu > ul.menu-items > li.item.active')
+let activeTab = document.querySelector('body > main.main > section.sidebar > .menu > ul.menu-items > li.item.active')
 const contentContainer = document.querySelector('body > main.main > section.content > #content-container')
 
-if (tabs && contentContainer && activeTab) {
+const content = {}
+
+content.overview = `
+  <stat-container bookings="163" date="Sep 23" completed="57" upcoming="33" cancelled="8" visitors="1,400"
+    interactions="1,360">
+  </stat-container>
+
+`
+
+content.schedules = `
+  <schedule-container url="some-link">
+  </schedule-container>
+`
+
+content.bookings = `
   
+
+`
+
+if (tabs && contentContainer && activeTab) {
+  tabs.forEach(tab => {
+    tab.addEventListener('click', (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+
+      activeTab.classList.remove('active')
+      tab.classList.add('active')
+      activeTab = tab
+    })
+  })
+}
+
+const populateContent = (contentContainer, content) => {
+  contentContainer.innerHTML = content
 }

953cbc64fc4388aa87be3ab45d2eeb69d8e0a0ad
fescii <isfescii@gmail.com>
2023-10-01 12:59:10 +0300

    Dashboard - Entry 306

---
+++ js/tab.js
@@ -0,0 +1,7 @@
+const tabs = document.querySelectorAll('body > main.main > section.sidebar > .menu > ul.menu-items > li.item')
+const activeTab = document.querySelector('body > main.main > section.sidebar > .menu > ul.menu-items > li.item.active')
+const contentContainer = document.querySelector('body > main.main > section.content > #content-container')
+
+if (tabs && contentContainer && activeTab) {
+  
+}
